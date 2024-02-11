20ed555de9d6eba4bdf39c207339b3518eff15bb
imadaabdrai <148750822+imadaabdrai@users.noreply.github.com>
2024-02-11 19:30:54 +0100

    Update downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
-    let response = `┏━━⊜ * 𝐀𝐏𝐊 𝐈𝐍𝐅𝐎*\n┃⋄ 𝗡𝗮𝗺𝗲 *:*\n┃➪ ${data5.name}\n┃⋄ 𝗣𝗮𝗰𝗸𝗮𝗴𝗲 *:*\n┃➪ ${data5.package}\n┃⋄ 𝗦𝗶𝘇𝗲 *:*\n┃➪ ${data5.size\n┗━━━━━━━━⬣`
+    let response = `*N𝖺𝗆𝖾:* ${data5.name}\n*Package:* ${data5.package}\n\n𝖬𝖾𝖾9 𝖮𝖿𝖿𝗂𝖼𝗂𝖺𝗅 🐋`
     await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 100) {
       return await conn.sendMessage(m.chat, {text: '*[ ⛔ ] الحجم اكبر من 100 ميغا لا يمكن ارسالة لحماية البوت من البان.*'}, {quoted: m});
@@ -15,5 +15,4 @@ const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
   }    
 };
 handler.command = /^(apk)$/i;
-handler.premium = false
 export default handler;
