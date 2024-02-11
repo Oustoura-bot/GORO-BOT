fc09c6df36b2de75c96f34a883d4cd002a7f81a4
imadaabdrai <148750822+imadaabdrai@users.noreply.github.com>
2024-02-11 19:13:49 +0100

    Update downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -1,18 +1,19 @@
 import {search, download} from 'aptoide-scraper';
 const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
- if (!text) throw `*[❗] Ingrese el nombre de la APK que quiera buscar.*`;
+ if (!text) throw `*[❗] 𝖠𝖽𝖽 𝖠𝗉𝗉 𝖭𝖺𝗆𝖾.*`;
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
-    let response = `📲 *Descargador de Aptoide* 📲\n\n📌 *Nombre:* ${data5.name}\n📦 *Package:* ${data5.package}\n🕒 *Última actualización:* ${data5.lastup}\n📥 *Tamaño:* ${data5.size}`
+    let response = `┏━━⊜ * 𝐀𝐏𝐊 𝐈𝐍𝐅𝐎*\n┃⋄ 𝗡𝗮𝗺𝗲 *:*\n┃➪ ${data5.name}\n┃⋄ 𝗣𝗮𝗰𝗸𝗮𝗴𝗲 *:*\n┃➪ ${data5.package}\n┃⋄ 𝗦𝗶𝘇𝗲 *:*\n┃➪ ${data5.size\n┗━━━━━━━━⬣`
     await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
- if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-      return await conn.sendMessage(m.chat, {text: '*[ ⛔ ] El archivo es demasiado pesado por lo que no se enviará.*'}, {quoted: m});
+ if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 100) {
+      return await conn.sendMessage(m.chat, {text: '*[ ⛔ ] الحجم اكبر من 100 ميغا لا يمكن ارسالة لحماية البوت من البان.*'}, {quoted: m});
     }
     await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
   } catch {
     throw `*[❗] Error, no se encontrarón resultados para su búsqueda.*`;
   }    
 };
-handler.command = /^(apkmod|modapk|dapk2|aptoide|aptoidedl)$/i;
+handler.command = /^(apk)$/i;
+handler.premium = false
 export default handler;
